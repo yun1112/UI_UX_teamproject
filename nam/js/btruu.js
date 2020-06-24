c567e55bdc8fb2ae79d53bed651385591d1648a6
YoungJoonNam <63032539+YoungJoonNam@users.noreply.github.com>
2020-06-25 03:11:06 +0900

    야옹

--- nam/js/btruu.js
+++ nam/js/btruu.js
@@ -3,3 +3,16 @@
 function goPage(url) {
     location.href = url;
 }
+
+function detailOrder(){            
+    document.getElementById('details').style.display='block';
+    return false;
+}
+function doClose(varCheck){    
+    if(varCheck=='order'){
+        alert('orders');
+
+    }                    
+    document.getElementById('details').style.display='none';
+    return false;
+}
